92c7caacde49fc58c00668c2a03a17c6847e589f
Marcin Kołodziej <marcinkolodziej15@gmail.com>
2019-02-03 17:21:44 +0100

    Updates

---
+++ precache-manifest.9df28513292009fff00f1c9acdfd693a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "38c34e5c600010fb9a7f",
+    "url": "/draft-js-playground/static/js/runtime~main.38c34e5c.js"
+  },
+  {
+    "revision": "7b72890b28b4953c81d7",
+    "url": "/draft-js-playground/static/js/main.7b72890b.chunk.js"
+  },
+  {
+    "revision": "d588de426cb5dded331c",
+    "url": "/draft-js-playground/static/js/1.d588de42.chunk.js"
+  },
+  {
+    "revision": "7b72890b28b4953c81d7",
+    "url": "/draft-js-playground/static/css/main.748c3204.chunk.css"
+  },
+  {
+    "revision": "d588de426cb5dded331c",
+    "url": "/draft-js-playground/static/css/1.63d969c5.chunk.css"
+  },
+  {
+    "revision": "d9b2b0ca1c29a1adc6e7bfee879037c0",
+    "url": "/draft-js-playground/index.html"
+  }
+];
